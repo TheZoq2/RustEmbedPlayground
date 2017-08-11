938548b18060b63404208d472554ded850c16b62
TheZoq2 <frans.skarman@gmail.com>
2017-08-11 21:08:12 +0200

    Make all leds light up

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -31,7 +31,16 @@ fn init(p: init::Peripherals, r: init::Resources) {
     p.RCC.ahbenr.modify(|_, w| w.iopeen().enabled());
 
     // Enable gpoie15
-    p.GPIOE.moder.modify(|_, w| w.moder15().output());
+    p.GPIOE.moder.modify(|_, w|
+                    w.moder8().output()
+                        .moder9().output()
+                        .moder10().output()
+                        .moder11().output()
+                        .moder12().output()
+                        .moder13().output()
+                        .moder14().output()
+                        .moder15().output()
+                    );
 
     // configure the system timer to generate one interrupt every second
     p.SYST.set_clock_source(SystClkSource::Core);
@@ -52,10 +61,27 @@ fn sys_tick(_t: &mut Threshold, r: SYS_TICK::Resources)
     **r.ON = !**r.ON;
 
     if **r.ON {
-        //r.GPIOC.bsrr.write(|w| w.bs13().set());
-        r.GPIOE.odr.modify(|_, w| w.odr15().set_bit())
+        r.GPIOE.odr.modify(|_, w| {
+            w.odr8().set_bit()
+                .odr9().set_bit()
+                .odr10().set_bit()
+                .odr11().set_bit()
+                .odr12().set_bit()
+                .odr13().set_bit()
+                .odr14().set_bit()
+                .odr15().set_bit()
+        })
     }
     else {
-        r.GPIOE.odr.modify(|_, w| w.odr15().clear_bit())
+        r.GPIOE.odr.modify(|_, w| {
+            w.odr8().clear_bit()
+                .odr9().clear_bit()
+                .odr10().clear_bit()
+                .odr11().clear_bit()
+                .odr12().clear_bit()
+                .odr13().clear_bit()
+                .odr14().clear_bit()
+                .odr15().clear_bit()
+        })
     }
 }
